9f7c7f57f6167cf0c1674ea2bca2930bb114201d
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-05-09 22:36:41 -0400

    Upload preprocessing configs for all characters

--- preprocess.bat
+++ preprocess.bat
@@ -1,4 +1,8 @@
 set PYTHONPATH=.
 set CUDA_VISIBLE_DEVICES=0 
-python preprocessing/binarize.py --config training/config.yaml
+
+for %%a in (hski ttmr fktn amao kllj ssmk shro kcna hrnm) do (
+    python preprocessing/binarize.py --config training/config_%%a.yaml
+)
+
 pause
