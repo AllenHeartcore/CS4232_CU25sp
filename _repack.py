90c7998ec7014cfd05a02bce4b210c0040ca2f2c
Ziyuan Chen <ziyuan.chen@columbia.edu>
2025-05-14 16:25:17 -0400

    [Fix] Repack to compatible .csv and .npz instead

--- _repack.py
+++ _repack.py
@@ -1,5 +1,7 @@
 import pickle
 
+import numpy as np
+
 CHARACTERS = [
     "hski",
     "ttmr",
@@ -20,12 +22,7 @@ if __name__ == "__main__":
         data_p = pickle.load(open(f"gkms{c}_parselmouth.pkl", "rb"))
 
         metadata = data_c["metadata"]
-        assert metadata["nframe"].max() <= 65535
-        assert metadata["nframe"].min() >= 0
-        assert metadata["nphone"].max() <= 65535
-        assert metadata["nphone"].min() >= 0
-        metadata["nframe"] = metadata["nframe"].astype("uint16")
-        metadata["nphone"] = metadata["nphone"].astype("uint16")
+        assert (metadata == data_p["metadata"]).all().all()
 
         assert data_c["pitch"].max() <= 255
         assert data_c["pitch"].min() >= 0
@@ -41,7 +38,6 @@ if __name__ == "__main__":
         assert (data_c["hubert"] == data_p["hubert"]).all()
 
         data = {
-            "metadata": metadata,
             "pitch_crepe": data_c["pitch"].astype("uint8"),
             "pitch_parselmouth": data_p["pitch"].astype("uint8"),
             "f0_crepe": data_c["f0"],
@@ -53,7 +49,7 @@ if __name__ == "__main__":
             "hubert": data_c["hubert"],
         }
 
-        with open(f"gkms{c}.pkl", "wb") as f:
-            pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+        metadata.to_csv(f"gkms{c}.csv")
+        np.savez_compressed(f"gkms{c}.npz", **data)
 
-        print(f"Reduced gkms{c}")
+        print(f"Repacked gkms{c}")
